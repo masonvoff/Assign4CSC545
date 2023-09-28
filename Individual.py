d0d43802809acbbcb3396c4662c0ba8b0e24c046
offuttm <mason.offutt@ucdenver.edu>
2023-09-28 08:41:59 -0400

    changed Individual.py

--- Individual.py
+++ Individual.py
@@ -4,43 +4,40 @@ class Individual:
 
     def __init__(self,map):
         self.map = map# the map
-        self.fitness=0# fitness is cached and only updated on request whenever necessary
-        # TODO some representation of the genome of the individual
+        self.fitness=1# fitness is cached and only updated on request whenever necessary
         self.genome = len(map.states)
-        # TODO implement random generation of an individual
         self.genome = self.randomColors()
         self.updateFitness()
     # Updates the fitness value based on the genome and the map.
+    def singleColor(self,change):
+        #changes one color
+        colors = ["Red", "Blue", "Green", "Yellow"]
+        change = colors[random.randint(0,3)]
+        return change
+
     def randomColors(self):
+        #assigns colors randomly to states
         colors = ["Red", "Blue", "Green", "Yellow"]
         colorAssignments = {}
         random.shuffle(colors)
         for state in self.map.states:
-            color = colors.pop(0)
-            colorAssignments[state] = color
-            colors.append(color)
+            colorAssignments[state] = colors[random.randint(0,3)]
         return colorAssignments
 
     def calculateFitness(self):
         ones_count = 0
-        firstIn = 0
-        secondIn = 0
-        for all in self.genome:
-            #print(self.map.states[self.map.borders[ones_count].index1])
+        fitness = 0
+        for all in self.map.borders:
             firstIn = self.map.states[self.map.borders[ones_count].index1]
             secondIn = self.map.states[self.map.borders[ones_count].index2]
-            #print(firstIn,secondIn)
-            print("the values here: ",self.genome[firstIn], self.genome[secondIn])
             if self.genome[firstIn] == self.genome[secondIn]:
-                print("collision")
+                #if two states touching have same color add one fitness
+                fitness += 1
             ones_count += 1
-            #this is what i need to finish
-            #idea was to check all colors in genome to see if they are different
-        return ones_count
+        return fitness
 
     def updateFitness(self):
-        #TODO implement fitness function
-        self.fitness=self.calculateFitness()
+        self.fitness = self.calculateFitness()
 
     # Reproduces a child randomly from two individuals (see textbook).
     # x The first parent.
@@ -48,19 +45,31 @@ class Individual:
     # return The child created from the two individuals.
     def reproduce(self, x, y):
         child = Individual(self.map)  # Create a new individual with the same map
-        # TODO reproduce child from individuals x and y
+        child.genome = self.genome
+        counter = 0
+        for all in self.map.borders:
+            firstIn = self.map.states[self.map.borders[counter].index1]
+            secondIn = self.map.states[self.map.borders[counter].index2]
+            if self.genome[firstIn] == self.genome[secondIn] and y.genome[firstIn] != y.genome[secondIn]:
+                #if y has different second state and x has a collision then the childs second state is changed to y's state
+
+                child.genome[firstIn] = y.genome[firstIn]
+                child.genome[secondIn] = y.genome[secondIn]
+            else:
+                #randomly change the color if it cannot be merged
+                child.genome[firstIn] = self.singleColor(firstIn)
+                child.genome[secondIn] = self.singleColor(secondIn)
         child.updateFitness()
         return child
 
     # Randomly mutates the individual.
     def mutate(self):
-        # TODO implement random mutation of the individual
-        self.updateFitness()
+        #simply rerandomizes an individual
+        self.genome = self.randomColors()
 
-    # Checks whether the individual represents a valid goal state.
-    # return Whether the individual represents a valid goal state.
     def isGoal(self):
-        return self.fitness == len(self.map.borders)
+        if self.fitness == 0:
+            return True
 
     def printresult(self):
         print("Your result:")
@@ -70,3 +79,5 @@ class Individual:
         # Carolina: 0
         # South Carolina: 2
         # ...
+        print(self.fitness)
+        print(self.genome)
--- Main.py
+++ Main.py
@@ -6,6 +6,32 @@ from Map import Map
 from Population import Population
 
 # Author Fan Zhang
+def createBigMap():
+    # Creates a Map object with all of the states from bigstates.txt
+    map = Map()
+    uniqueStates = set()
+    uniqueBorders = set()
+    with open('bigstates.txt', 'r') as file:
+        for line in file:
+            stateCodes = line.strip().split(',')
+            # Add the first state code to the map's states list and the set
+            if stateCodes[0] not in uniqueStates:
+                map.states.append(stateCodes[0])
+                uniqueStates.add(stateCodes[0])
+            for i in range(1, len(stateCodes)):
+                # Add the next state code to the states list and the set
+                if stateCodes[i] not in uniqueStates:
+                    map.states.append(stateCodes[i])
+                    uniqueStates.add(stateCodes[i])
+                # Create a border between the first state and the following state if it doesn't exist
+                border = (map.states.index(stateCodes[0]), map.states.index(stateCodes[i]))
+                # Check if the border or its reverse already exists
+                if border not in uniqueBorders and (border[1], border[0]) not in uniqueBorders:
+                    map.borders.append(Border(*border))
+                    uniqueBorders.add(border)
+    return map
+
+
 def initMap(map):
     map.states.append("North Carolina")
     map.states.append("South Carolina")
@@ -37,12 +63,12 @@ def initMap(map):
     map.borders.append(Border(7, 9))
 
 if __name__ == '__main__':
+    #bigmap = createBigMap()
     map = Map()
     initMap(map)
-    populationSize = 100  # TODO find a reasonable value
+    populationSize = 10
     population = Population(map, populationSize)
-
-    maxIterations = 250  # TODO find a reasonable value
+    maxIterations = 20000
     currentIteration = 0
     goalFound = False
     bestIndividual = Individual(map)  # to hold the individual representing the goal, if any
@@ -52,7 +78,7 @@ if __name__ == '__main__':
             x = population.randomSelection()
             y = population.randomSelection()
             child = x.reproduce(x, y)
-            if False:  # TODO use a small probability instead
+            if random.randint(0,200) == 23:
                 child.mutate()
             if child.isGoal():
                 goalFound = True
